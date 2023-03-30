82420420c93344550ba6f3ccdac2168235c7037d
Ricardo Nava <ricardo1725@gmail.com>
2023-03-29 21:20:27 -0400

    Code Using info from indexTest.js only

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+let currentUser = 'string';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInital = currentUser[0];
+
+let shortGreeting = "Welcome, " + firstInital + "!";
